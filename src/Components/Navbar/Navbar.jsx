6e12f285eaa4623966cf16a35e67d8add5cd93e4
Ankit <ankitchauhan21500@gmail.com>
2023-04-04 23:20:18 +0530

    reduced image size

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,17 +8,18 @@ export default function Navbar(props) {
   }
   return (
     <>
-    <main id={props.id} className='Navbar_main'>
+      <main id={props.id} className='Navbar_main'>
         <div className="Navbar_main_div">
-            <img src={Logo} alt="" />
-            <ul className="Nav_UList">
-                <li className="Nav_list one"><a onClick={() => scroll('home')}>Home</a></li>
-                <li className="Nav_list"><a onClick={() => scroll('about')}>About Us</a></li>
-                <li className="Nav_list"><a onClick={() => scroll('contact')}>Contact</a></li>
-                <li className="Nav_list"> <a href="https://maps.app.goo.gl/4tVQ9gqLm7VymsoD6" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-location-dot"></i></a></li>
-            </ul>
+          <img src={Logo} alt="" />
+          <ul className="Nav_UList">
+            <li className="Nav_list one"><a onClick={() => scroll('home')}>Home</a></li>
+            <li className="Nav_list"><a onClick={() => scroll('about')}>About Us</a></li>
+            <li className="Nav_list"><a onClick={() => scroll('contact')}>Contact</a></li>
+            <li className="Nav_list"> <a href="https://maps.app.goo.gl/4tVQ9gqLm7VymsoD6" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-location-dot"></i></a></li>
+            <li className="Nav_list floating"><i onClick={() => scroll('home')} class="fa-solid fa-arrow-up"></i></li>
+          </ul>
         </div>
-    </main>
+      </main>
     </>
   )
 }
