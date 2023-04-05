65c94163eb50ffe272303e2640b38e198abf12ff
Ankit <ankitchauhan21500@gmail.com>
2023-04-05 15:48:11 +0530

    improved nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,12 +12,12 @@ export default function Navbar(props) {
         <div className="Navbar_main_div">
           <img src={Logo} alt="" />
           <ul className="Nav_UList">
+            <li className="Nav_list floating"><i onClick={() => scroll('home')} class="fa-solid fa-arrow-up"></i></li>
             <li className="Nav_list one"><a onClick={() => scroll('home')}>Home</a></li>
             <li className="Nav_list"><a onClick={() => scroll('about')}>About Us</a></li>
             <li className="Nav_list"><a onClick={() => scroll('trainer')}>Trainers</a></li>
             <li className="Nav_list"><a onClick={() => scroll('contact')}>Contact</a></li>
             <li className="Nav_list"> <a href="https://maps.app.goo.gl/4tVQ9gqLm7VymsoD6" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-location-dot"></i></a></li>
-            <li className="Nav_list floating"><i onClick={() => scroll('home')} class="fa-solid fa-arrow-up"></i></li>
           </ul>
         </div>
       </main>
