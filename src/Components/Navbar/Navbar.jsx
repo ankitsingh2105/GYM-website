8b4b37eea5bf5aaa0366104a72dbb28e1fec60c5
Ankit <ankitchauhan21500@gmail.com>
2023-04-07 10:17:47 +0530

    improved

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar(props) {
             <li className="Nav_list"><a onClick={() => scroll('about')}>About Us</a></li>
             <li className="Nav_list"><a onClick={() => scroll('trainer')}>Trainers</a></li>
             <li className="Nav_list"><a onClick={() => scroll('contact')}>Contact</a></li>
-            <li className="Nav_list"> <a href="https://maps.app.goo.gl/4tVQ9gqLm7VymsoD6" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-location-dot"></i></a></li>
+            <li className="Nav_list"> <a href="https://.app.goo.gl/4tVQ9gqLm7VymsoD6" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-location-dot"></i></a></li>
           </ul>
         </div>
       </main>
